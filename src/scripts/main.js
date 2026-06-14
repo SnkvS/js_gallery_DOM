85ff0f1ea1c82735eb319c3518701afadd0aa567
Serhii Senkiv <sergei.senkiv6@gmail.com>
2026-06-14 14:41:25 +0300

    Implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const image = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
+image.addEventListener('click', (event) => {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  event.preventDefault();
+  largeImage.src = link.href;
+  largeImage.alt = link.alt;
+});
